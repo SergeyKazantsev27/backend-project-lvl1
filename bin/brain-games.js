f8d93de78ed784e6d83d8c6639fad77684878466
Sergey Kazantsev <SergeyKazantsev27@gmail.com>
2021-11-10 02:26:36 +0300

    sayMyName func from readline lib

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import sayMyName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!')
+sayMyName();
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from "readline-sync";
+
+const sayMyName = () => {
+    const name = readlineSync.question('Your answer: ');
+    console.log(`Hello, ${name}!`);
+}
+
+export default sayMyName;
