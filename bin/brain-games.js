730cd6049d1edf9dbb2e5ff96ffea9d310cfd01b
Sergey Kazantsev <SergeyKazantsev27@gmail.com>
2021-11-12 02:41:50 +0300

    upd: brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,4 @@
 
 import sayMyName from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
 sayMyName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const sayMyName = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('Your answer: ');
   console.log(`Hello, ${name}!`);
 };
