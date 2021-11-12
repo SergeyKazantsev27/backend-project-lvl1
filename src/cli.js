8e0f4c1c54ef06fc45fd5f34f85cd2c71946e082
Sergey Kazantsev <SergeyKazantsev27@gmail.com>
2021-11-12 03:43:40 +0300

    upd: welcome message

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 const sayMyName = () => {
   console.log('Welcome to the Brain Games!');
+  console.log('May I have your name?');
   const name = readlineSync.question('Your answer: ');
   console.log(`Hello, ${name}!`);
 };
