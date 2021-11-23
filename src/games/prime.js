61b85c81ac55f03cf60ebfd9d691fb007a564d89
Sergey Kazantsev <SergeyKazantsev27@gmail.com>
2021-11-24 01:48:04 +0300

    refactor: code optimisation

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,28 +1,48 @@
-import game from '../index.js';
+import gameLogic, { userName } from '../index.js';
 
+// game description
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+// round rules
+let currentRound = 0;
+const maxRound = 3;
+
+// game logic
+const isPrime = (num) => {
+  if (num === 1) {
+    return true;
+  }
+
+  for (let i = 2; i < num; i += 1) {
+    if ((num % i) === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// generate rounds
+const generateRound = () => {
+  const question = (Math.floor(Math.random() * 100) + 1);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return gameLogic(gameDescription, question, correctAnswer);
+};
+
+// run rounds
 const runGame = () => {
-  game.run(
-    'Answer "yes" if given number is prime. Otherwise answer "no".',
-    (() => {
-      // eslint-disable-next-line consistent-return
-      const isPrime = (num) => {
-        if (num === 1) {
-          return true;
-        }
+  console.log(gameDescription);
 
-        for (let i = 2; i < num; i += 1) {
-          if ((num % i) === 0) {
-            return false;
-          }
-        }
-        return true;
-      };
+  while (currentRound < maxRound) {
+    if (generateRound()) {
+      currentRound += 1;
+    } else {
+      break;
+    }
+  }
 
-      const question = (Math.floor(Math.random() * 100) + 1);
-      const answer = (isPrime(question) === true) ? 'yes' : 'no';
-      return [question, answer];
-    }),
-  );
+  if (currentRound === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
 };
 
 export default runGame;
