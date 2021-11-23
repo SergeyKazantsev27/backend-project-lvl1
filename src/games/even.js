954da9ec3ae512efe6d156bfaaca0bf71a4e55f9
Sergey Kazantsev <SergeyKazantsev27@gmail.com>
2021-11-23 15:42:41 +0300

    refactor: optimize code

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,37 @@
-import game from '../index.js';
+import gameLogic, { userName } from '../index.js';
 
+// game description
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+// round rules
+let score = 0;
+const maxScore = 3;
+
+// game logic
 const isEven = (num) => (num % 2) === 0;
 
+// generate rounds
+const generateRound = () => {
+  const question = Math.floor(Math.random() * 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return gameLogic(gameDescription, question, correctAnswer);
+};
+
+// run rounds
 const runGame = () => {
-  game.run(
-    'Answer "yes" if the number is even, otherwise answer "no".',
-    (() => {
-      const question = Math.floor(Math.random() * 100);
-      const answer = (isEven(question) === true) ? 'yes' : 'no';
-      return [question, answer];
-    }),
-  );
+  console.log(gameDescription);
+
+  while (score < maxScore) {
+    if (generateRound() === true) {
+      score += 1;
+    } else {
+      break;
+    }
+  }
+
+  if (score === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
 };
 
 export default runGame;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 import readlineSync from 'readline-sync';
 import sayMyName from './cli.js';
 
-let score = 0;
+export const userName = sayMyName();
 
-const game = {
-  askQuestion(callback) {
-    if (score === 3) {
-      console.log(`Congratulations, ${game.userName}!`);
-    }
+const gameLogic = (gameDescription, question, correctAnswer) => {
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
 
-    if (score < 3) {
-      const [gameQuestion, correctAnswer] = callback();
-
-      console.log(`Question: ${gameQuestion}`);
-      const userAnswer = readlineSync.question('Your answer: ');
-
-      if (userAnswer === correctAnswer) {
-        console.log('Correct!');
-        score += 1;
-        game.askQuestion(callback);
-      } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-        console.log(`Let's try again, ${game.userName}!`);
-      }
-    }
-  },
-  run(title, callback) {
-    game.userName = sayMyName();
-    console.log(title);
-    game.askQuestion(callback);
-  },
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${userName}!`);
+  return false;
 };
 
-export default game;
+export default gameLogic;
